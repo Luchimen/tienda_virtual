55698433fd5c155284ed1b967637dee264d889f2
kevinflorestacna <kevinflorestacna@gmail.com>
2022-07-20 20:14:17 -0500

    Enlace del .json al html con javascript

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-import products from "./products.json" assert { type: "json" };
-
-let productosTienda = products;
-
-productosTienda.map((product) => {
-  const imgSrc = product.image.original;
-  let img = document.createElement("img");
-  img.src = imgSrc;
-  const cardImagen = `<section class="card">
-  <img src=${imgSrc} alt="Imagen de prueba" />
-  <h3>Nombre de producto</h3>
-  <p>Descripcion</p>
-  <p>Precio</p>
-</section>`;
-  const div = document.createElement("div");
-  div.innerHTML = cardImagen;
-  document.querySelector("gridImages").append(div.firstElementChild);
-});
+const items=document.getElementById('items')
+const template=document.getElementById('template').content
+const fragment=document.createDocumentFragment()
+document.addEventListener('DOMContentLoaded',()=>{
+    fetchData()
+})
+const fetchData=async() =>{
+    try{
+        const res=await fetch('products.json')
+        const data=await res.json()
+        printcard(data)
+        //console.log(data)
+    }catch (error){
+        console.log(error)
+    }
+}
+const printcard=data=>{
+    data.forEach(productos => {
+        console.log(productos)
+        template.querySelector('h3').textContent=productos.name
+        template.querySelector('p','.descripcion').textContent=productos.description
+        template.querySelector('img').setAttribute("src",productos.image.thumbnail)
+        template.querySelector('#precio').textContent=productos.price
+        const clone=template.cloneNode(true)
+        fragment.appendChild(clone)
+        console.log(template)
+    });
+    items.appendChild(fragment)
+}
